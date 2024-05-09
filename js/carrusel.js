a2bd4fb9cfe27c56b2c44e4c5b66c36035bd26ef
Andres <154449407+DarkZel2@users.noreply.github.com>
2024-05-09 12:25:28 -0500

    update

--- js/carrusel.js
+++ js/carrusel.js
@@ -3,8 +3,12 @@ const btnLeft = document.querySelector(".btn-left"),
       slider = document.querySelector("#slider"),
       sliderSection = document.querySelectorAll(".section-carrusel");
 
-btnLeft.addEventListener("click", e => moveToLeft());
-btnRight.addEventListener("click", e => moveToRight());
+btnLeft.addEventListener("click", e => moveToLeft())
+btnRight.addEventListener("click", e => moveToRight())
+
+setInterval(() =>{
+  moveToRight()
+}, 3000);
 
 let operacion = 0,
     counter = 0,
@@ -15,19 +19,21 @@ function moveToRight() {
     counter = 0;
     operacion = 0;
     slider.style.transform = `translateX(-${operacion}%)`
-  } else {
-    counter++
-    operacion = operacion + widthImg;
-    slider.style.transform = `translateX(-${operacion}%)`
-    slider.style.transition = "all ease .6s"
+    return;
   }
-  
+  counter++
+  operacion = operacion + widthImg;
+  slider.style.transform = `translateX(-${operacion}%)`
+  slider.style.transition = "all ease .6s"
 }
 
 function moveToLeft() {
   counter--;
   if (counter < 0) {
+    counter = sliderSection.length - 1;
+    operacion = widthImg * (sliderSection.length-1);
     slider.style.transform = `translateX(-${operacion}%)`
+    return;
   }
   operacion = operacion - widthImg;
   slider.style.transform = `translateX(-${operacion}%)`
