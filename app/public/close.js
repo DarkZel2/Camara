a04abc7e9459c836d9b6fa50a801caf18b7bdaad
DarkZel2 <154449407+DarkZel2@users.noreply.github.com>
2024-05-30 13:03:39 -0500

    update

--- app/public/close.js
+++ app/public/close.js
@@ -1,4 +1,4 @@
-document.getElementById("close-log")[0].addEventListener("click", () => {
+document.getElementById("close-log").addEventListener("click", () => {
   document.cookie ='jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
   document.location.href = "/login";
 });
