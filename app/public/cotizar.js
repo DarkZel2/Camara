fc4341067955cb4ef5157137f77771d864e8d98d
DarkZel2 <154449407+DarkZel2@users.noreply.github.com>
2024-07-02 14:52:48 -0500

    update

--- app/public/cotizar.js
+++ app/public/cotizar.js
@@ -15,29 +15,17 @@ const salones =
   capacity: 200,
   price: {
     hora: {
-      lunes: 164000,
-      martes: 164000,
-      miercoles: 164000,
-      jueves: 164000,
-      viernes: 184000,
-      sabado: 184000
+      week: 164000,
+      weekend: 184000
     },
     horas: {
       afiliado: {
-        lunes: 878000,
-        martes: 878000,
-        miercoles: 878000,
-        jueves: 878000,
-        viernes: 1010000,
-        sabado: 1010000
+        week: 878000,
+        weekend: 1010000
       },
       particular: {
-        lunes: 1145000,
-        martes: 1145000,
-        miercoles: 1145000,
-        jueves: 1145000,
-        viernes: 1145000,
-        sabado: 1145000
+        week: 1145000,
+        weekend: 1145000
       }
     }
   },
@@ -51,41 +39,146 @@ const salones =
   }
 }
 
-const valorMesas = salones.servicios.mesas;
-const valorSonidoP = salones.servicios.sonidoPro;
-const valorSonidoE = salones.servicios.sonidoEx;
-const valorBanderas = salones.servicios.banderas;
-const valorManteles = salones.servicios.manteles;
-const valorMaquina = salones.servicios.maquina;
+// Funciona para buscar el dia de la semana correcpondiente a una fecha
+function traerDiaSemana(fecha) {
+  let date = new Date(fecha);
+  let day = date.getDay() + 1;
 
-const checks =  document.querySelectorAll(".formulario__checkbox");
-const buttom =  document.getElementById("event-form");
-const lista = document.getElementById("lista");
-
-let seleccionados = [];
-
-function insertSelect(value) {
-  const comprobar = seleccionados.find();
-  if (!comprobar) {
-    seleccionados.push(value);
+  switch (day) {
+    case 1:
+      return 'Lunes';
+    case 2:
+      return 'Martes';
+    case 3:
+      return 'Miércoles';
+    case 4:
+      return 'Jueves';
+    case 5:
+      return 'Viernes';
+    case 6:
+      return 'Sábado';
+    case 7:
+      return 'Domingo';
+    default:
+      return 'Fecha no encontrada.'
   }
 }
+
+const checks = document.querySelectorAll(".formulario__checkbox");
+const inputs = document.querySelectorAll(".formulario__input");
+const radios = document.querySelectorAll(".formulario__radio");
+const times = document.querySelectorAll(".formulario__time");
+const date = document.querySelectorAll(".formulario__date");
+const buttom = document.getElementById("event-form");
+const listaFecha = document.getElementById("dateList");
+const listaHora = document.getElementById("timeList");
+const listaTipoEvento = document.getElementById("typeEventList");
+const listaServicios = document.getElementById("listaServicios");
+const listaDatos = document.getElementById("listaDatos");
+
+// Funcionabilidad de la fecha
+const validarFecha = (e) => {
+  const diaEvento = traerDiaSemana(e.target.value);
+  listaFecha.innerHTML = "";
+  if (e.target.value !== "") {
+    var elemento = document.createElement('li');
+    elemento.innerHTML = `${diaEvento} ${e.target.value}`;
+    listaFecha.appendChild(elemento);
+  };
+
+  var valor = "";
+  if (
+    diaEvento === "Lunes" ||
+    diaEvento === "Martes" ||
+    diaEvento === "Miércoles" ||
+    diaEvento === "Jueves"
+  ) {
+    valor = salones.price.hora.week;
+  } else if (
+    diaEvento === "Viernes" ||
+    diaEvento === "Sábado"
+  ) {
+    valor = salones.price.hora.weekend;
+  }
+  return valor;
+};
+
+date.forEach((date) => {
+  date.addEventListener('blur', validarFecha)
+});
+// Funcionabilidad de la hora
+const validarHora = (e) => {
+  listaHora.innerHTML = "";
+  times.forEach((e) => {
+    if (e.value !== "") {
+      var elemento = document.createElement('li');
+      elemento.innerHTML = `Hora de ${e.id} ${e.value}`;
+      listaHora.appendChild(elemento);
+    }
+  })
+
+  console.log(e.target.value)
+};
+
+times.forEach((time) => {
+  time.addEventListener('keyup', validarHora)
+  time.addEventListener('blur', validarHora)
+})
+// Funcionabilidad de los checks de tipo de evento
+const validarEvento = (e) => {
+  listaTipoEvento.innerHTML = "";
+  radios.forEach((e) => {
+    if (e.checked == true) {
+      var elemento = document.createElement('li');
+      elemento.innerHTML = `Evento ${e.value}`;
+      listaTipoEvento.appendChild(elemento);
+    }
+  })
+};
+
+radios.forEach((check) => {
+  check.addEventListener('click', validarEvento)
+});
+// FUncionabilidad de lis checkbox de servicios
+const validarChecks = (e) => {
+  listaServicios.innerHTML = "";
+  checks.forEach((e) => {
+    if (e.checked == true) {
+      var elemento = document.createElement('li');
+      elemento.innerHTML = `${e.value}`;
+      listaServicios.appendChild(elemento);
+    }
+  });
+};
+
+checks.forEach((check) => {
+  check.addEventListener('click', validarChecks)
+});
+
+// Funcionalidades de los inputs de datos del encargado
+const validarInputs = (e) => {
+  listaDatos.innerHTML = "";
+  inputs.forEach((e) => {
+    if (e.value !== "") {
+      var elemento = document.createElement('li');
+      elemento.innerHTML = e.value;
+      listaDatos.appendChild(elemento);
+    }
+  });
+};
+
+inputs.forEach((input) => {
+  input.addEventListener('keyup', validarInputs);
+  input.addEventListener('blur', validarInputs);
+});
+
+
+
+
 
 buttom.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  lista.innerHTML = "";
-  checks.forEach((e) => {
-    if (e.checked == true){
-      insertSelect(e.value);
-      var elemento = document.createElement('li');
-      elemento.innerHTML = e.value;
-      lista.appendChild(elemento);
-      return seleccionados;
-    }
-  });
-  console.log(seleccionados)
-  console.log(salones.servicios)
 
   // const res = await fetch("http://localhost:4500/api/cotizar", {
   //   method:"POST",
